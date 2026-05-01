0e5b249652fd411678f4e4b1d6f6dc6b28b7613a
Raven-21 <faithseeker917@outlook.com>
2026-05-01 21:34:27 +0800

    first commit

---
+++ 05-while loop.py
@@ -0,0 +1,5 @@
+count = 0
+
+while count < 5:
+    print("Check", count)
+    count += 1
---
+++ day1.py
@@ -0,0 +1,9 @@
+#Method 1
+name = "Tom"
+age = 20
+print("Hello, my name is", name + ". I'm", age, "years old.")
+
+#Method 2
+name = "Tom"
+age = 20
+print(f"hello, my name is {name}. I'm {age} years old.")
