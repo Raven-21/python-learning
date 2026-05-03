ca9781b20448899ba59542697336def61a83497f
Raven-21 <faithseeker917@outlook.com>
2026-05-03 23:17:39 +0800

    Continue improving Guess the Number

--- day2 - Guess the Number.py
+++ day2 - Guess the Number.py
@@ -4,13 +4,29 @@ print("You have 10 chances. Good luck!")
 
 # Generate a random number
 import random
-
 number = random.randint(1,100)
 #print(number)
 
 # Number of records
 count = 0
 chance = 10
+
+# Change of count
+def show_count(current_count):
+    if current_count == 1:
+        print(f"You guessed it {current_count} time")
+    else:
+        print(f"You guessed it {current_count} times")
+
+# Change of chance
+def show_chance(current_chance):
+    if current_chance > 1:
+        print(f"You have {current_chance} more chances")
+    elif current_chance == 1:
+        print("You only get one chance. Keep it up!")
+    else:
+        print("Sorry! You didn't guess correctly within the allotted number of attempts. - -!")
+        print(f"The answer is {number}")
 
 while True:
 # Get user input
@@ -20,35 +36,15 @@ while True:
 # Determination
     if guess > number:
         print("Too high")
-        # Change of records
-        if count == 1:
-            print(f"You guessed it {count} time")
-        else:
-            print(f"You guessed it {count} times")
-        # Change of chances
-        if (chance < 10) and (chance > 0) and chance != 1:
-            print(f"You have {chance} more chances")
-        elif chance == 1:
-            print("You only get one chance\nKeep it up!")
-        else:
-            print("Sorry! You didn't guess correctly within the allotted number of attempts. - -!")
-            print(f"The answer is {number}")
+        show_count(count)
+        show_chance(chance)
+        if chance == 0:
             break
     elif guess < number:
         print("Too low")
-        # Change of records
-        if count == 1:
-            print(f"You guessed it {count} time")
-        else:
-            print(f"You guessed it {count} times")
-        # Change of chances
-        if (chance < 10) and (chance > 0) and chance != 1:
-            print(f"You have {chance} more chances")
-        elif chance == 1:
-            print("You only get one chance\nKeep it up!")
-        else:
-            print("Sorry! You didn't guess correctly within the allotted number of attempts. - -!")
-            print(f"The answer is {number}")
+        show_count(count)
+        show_chance(chance)
+        if chance == 0:
             break
     else:
         print("You guessed it right ✔")
